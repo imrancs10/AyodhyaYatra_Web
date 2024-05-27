dffdb37235d6fa89632e6a09caa3e9375196bd01
Satish Kumar Sonker <btech.csit@gmail.com>
2024-05-27 09:06:41 +0530

    Visitor Registration Done

--- AyodhyaYatra_Web/Models/DashboardCountModel.cs
+++ AyodhyaYatra_Web/Models/DashboardCountModel.cs
@@ -10,12 +10,18 @@ namespace AyodhyaYatra_Web.Models
         public int id { get; set; }
         public int yatraCount { get; set; }
         public int registrationCount { get; set; }
-        public List<string> monthlyVisitorCounts { get; set; }
+        public List<MonthlyVisitorCountModel> monthlyVisitorCounts { get; set; }
         public List<attractionCountsModel> attractionCounts { get; set; }
     }
     public class attractionCountsModel
     {
         public string attractionType { get; set; }
         public int count { get; set; }
+    }
+
+    public class MonthlyVisitorCountModel
+    {
+        public int MonthYear { get; set; }
+        public int Count { get; set; }
     }
 }
